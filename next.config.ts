cdabf641bf9a2d8b11891b2e15b8065f184fdd2b
Devesh Patil <155056579+devpatony@users.noreply.github.com>
2025-03-05 22:49:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -55,7 +55,7 @@ export default withSentryConfig(nextConfig, {
   // tunnelRoute: "/monitoring",
 
   // Hides source maps from generated client bundles
-
+  sourcemaps: {},
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
 
